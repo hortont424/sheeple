4a5852dab6bc0f765752a031b517e62abfc0a66b
Tim Horton <hortont424@gmail.com>
2009-10-01 21:44:49 -0400

    Reimplement source view with normal Gtk packing model instead of TreeView

--- src/sheeple/main.c
+++ src/sheeple/main.c
@@ -17,12 +17,6 @@
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdio.h>
-
 #include <config.h>
 
 #include <gtk/gtk.h>
@@ -30,131 +24,103 @@
 
 #include <libsheeple/sheeple.h>
 
-#include "SheepleSourceViewCellRenderer.h"
-
-static GtkTreeStore *treestore;
-
-enum {
-    COL_SOURCES = 0,
-    NUM_COLS
-};
-
-GtkWidget *create_view_and_model(void)
+GtkWidget *create_source_view_test(GHashTable *sources)
 {
-    GtkTreeViewColumn *col;
-    GtkCellRenderer *renderer;
-    GtkTreeIter iter, toplevel;
-    GtkWidget *view;
-    GdkColor bg_color;
-    GtkTreeSelection *selection;
+    GtkWidget *scrollbox, *layout, *master_vbox, *master_padding;
+    GList *current_source_buttons = NULL;
+    GHashTableIter iter;
+    gpointer source_title, source_details;
     
-    SheepleSource * firstSource = (SheepleSource *)calloc(1, sizeof(SheepleSource));
-    SheepleSource * subSource = (SheepleSource *)calloc(1, sizeof(SheepleSource));
-    SheepleSource * subSource2 = (SheepleSource *)calloc(1, sizeof(SheepleSource));
-    SheepleSource * subSource3 = (SheepleSource *)calloc(1, sizeof(SheepleSource));
-    SheepleSource * subSource4 = (SheepleSource *)calloc(1, sizeof(SheepleSource));
-    SheepleSource * subSource5 = (SheepleSource *)calloc(1, sizeof(SheepleSource));
-    SheepleSource * subSource6 = (SheepleSource *)calloc(1, sizeof(SheepleSource));
-    SheepleSource * toplevel2 = (SheepleSource *)calloc(1, sizeof(SheepleSource));
+    scrollbox = gtk_scrolled_window_new(NULL, NULL);
+    layout = gtk_layout_new(NULL, NULL);
+    master_vbox = gtk_vbox_new(FALSE, 10);
     
-    firstSource->name = "My Contacts";
-    firstSource->toplevel = 1;
-    subSource->name = "RPI Friends";
-    subSource->toplevel = 0;
-    subSource2->name = "Family";
-    subSource2->toplevel = 0;
-    subSource2->selected = 1;
-    subSource3->name = "Sheeple";
-    subSource3->toplevel = 0;
-    subSource4->name = "Mike's Family";
-    subSource4->toplevel = 0;
-    subSource5->name = "RCOS";
-    subSource5->toplevel = 0;
+    // Iterate over all sources
+    g_hash_table_iter_init(&iter, sources);
+    while(g_hash_table_iter_next(&iter, &source_title, &source_details))
+    {
+        GtkWidget *sourcebox, *title_label;
+        gchar *title_markup;
+        GList *group_list = (GList *)source_details;
+        
+        sourcebox = gtk_vbox_new(FALSE, 0);
+        
+        // Create the source's title label
+        title_markup = g_markup_printf_escaped("<span weight=\"bold\" size=\"small\">%s</span>",
+                                               (const gchar *)source_title);
+        title_label = gtk_label_new(NULL);
+        gtk_label_set_markup(GTK_LABEL(title_label), title_markup);
+        gtk_misc_set_alignment(GTK_MISC(title_label), 0, 0);
+        gtk_box_pack_start(GTK_BOX(sourcebox), title_label, TRUE, TRUE, 4);
+        
+        // Iterate over all groups within this source, creating
+        // selection buttons for each group and packing them
+        do
+        {
+            GtkWidget *button, *alignment, *button_label;
+            gchar *button_markup;
+            
+            alignment = gtk_alignment_new(0, 0, 0, 0);
+            gtk_alignment_set_padding(GTK_ALIGNMENT(alignment), 0, 0, 20, 4);
+            
+            button_markup = g_markup_printf_escaped("<span size=\"small\">%s</span>",
+                                                    (const gchar *)group_list->data);
+            
+            button_label = gtk_label_new(NULL);
+            gtk_label_set_markup(GTK_LABEL(button_label), button_markup);
+            
+            button = gtk_button_new();
+            gtk_container_add(GTK_CONTAINER(button), button_label);
+            gtk_button_set_relief(GTK_BUTTON(button), GTK_RELIEF_NONE);
+            gtk_button_set_focus_on_click(GTK_BUTTON(button), FALSE);
+            
+            gtk_container_add(GTK_CONTAINER(alignment), button);
+            gtk_box_pack_start(GTK_BOX(sourcebox), alignment, TRUE, TRUE, 0);
+        }
+        while(group_list = g_list_next(group_list));
+        
+        gtk_box_pack_start(GTK_BOX(master_vbox), sourcebox, FALSE, TRUE, 0);
+    }
     
-    toplevel2->name = "Matt's Friends";
-    toplevel2->toplevel = 1;
-    subSource6->name = "Two girls";
-    subSource6->toplevel = 0;
+    //gtk_layout_put(GTK_LAYOUT(layout), master_vbox, 0, 0);
+    //gtk_container_add(GTK_CONTAINER(scrollbox), layout);
+    
+    // Pad the whole sourceview
+    master_padding = gtk_alignment_new(0, 0, 1, 1);
+    gtk_alignment_set_padding(GTK_ALIGNMENT(master_padding), 0, 0, 4, 4);
+    gtk_container_add(GTK_CONTAINER(master_padding), master_vbox);
 
-    treestore = gtk_tree_store_new(NUM_COLS, G_TYPE_POINTER);
-    gtk_tree_store_append(treestore, &iter, NULL);
-    toplevel = iter;
-    gtk_tree_store_set(treestore, &iter, COL_SOURCES, (gpointer)firstSource, -1);
-    gtk_tree_store_append(treestore, &iter, &toplevel);
-    gtk_tree_store_set(treestore, &iter, COL_SOURCES, (gpointer)subSource, -1);
-    gtk_tree_store_append(treestore, &iter, &toplevel);
-    gtk_tree_store_set(treestore, &iter, COL_SOURCES, (gpointer)subSource2, -1);
-    gtk_tree_store_append(treestore, &iter, &toplevel);
-    gtk_tree_store_set(treestore, &iter, COL_SOURCES, (gpointer)subSource3, -1);
-    gtk_tree_store_append(treestore, &iter, &toplevel);
-    gtk_tree_store_set(treestore, &iter, COL_SOURCES, (gpointer)subSource4, -1);
-    gtk_tree_store_append(treestore, &iter, &toplevel);
-    gtk_tree_store_set(treestore, &iter, COL_SOURCES, (gpointer)subSource5, -1);
-    gtk_tree_store_append(treestore, &iter, NULL);
-    gtk_tree_store_set(treestore, &iter, COL_SOURCES, (gpointer)toplevel2, -1);
-    toplevel = iter;
-    gtk_tree_store_append(treestore, &iter, &toplevel);
-    gtk_tree_store_set(treestore, &iter, COL_SOURCES, (gpointer)subSource6, -1);
-    
-    view = gtk_tree_view_new_with_model(GTK_TREE_MODEL(treestore));
-
-    g_object_unref(treestore);  /* destroy store automatically with view */
-
-    renderer = sheeple_source_view_cell_renderer_new();
-    col = gtk_tree_view_column_new();
-    gtk_tree_view_column_pack_start(col, renderer, TRUE);
-    gtk_tree_view_column_add_attribute(col, renderer, "source", COL_SOURCES);
-    gtk_tree_view_append_column(GTK_TREE_VIEW(view), col);
-
-    gtk_tree_view_set_headers_visible(GTK_TREE_VIEW(view), FALSE);
-    
-    // Set background; eventually, get this from the theme!!
-    gdk_colormap_alloc_color(gdk_colormap_get_system(), &bg_color, TRUE, TRUE);
-    gdk_color_parse("#d7dbd4", &bg_color);
-    gtk_widget_modify_base(view, GTK_STATE_NORMAL, &bg_color);
-    
-    // Hide expanders (expand everything by default), but keep 12px indent
-    // Eventually I'll have to solve expanders, probably, but not today.
-    gtk_tree_view_set_show_expanders(GTK_TREE_VIEW(view), FALSE);
-    gtk_tree_view_set_level_indentation(GTK_TREE_VIEW(view), 12);
-    gtk_tree_view_expand_all(GTK_TREE_VIEW(view));
-    
-    // Disable selection of anything in the treeview
-    selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(view));
-    gtk_tree_selection_set_mode(selection, GTK_SELECTION_NONE);
-
-    return view;
+    return master_padding;
 }
 
 int main(int argc, char **argv)
 {
-    GtkWidget *window, *view, *hbox, *button;
-    GtkTreeSelection *sel;
+    GtkWidget *window;
+    GHashTable *sources = NULL;
+    GList *contacts_list = NULL, *matt_list = NULL;
 
     gtk_init(&argc, &argv);
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_default_size(GTK_WINDOW(window), 150, 500);
+    gtk_window_set_default_size(GTK_WINDOW(window), 150, 300);
     g_signal_connect(window, "delete_event", gtk_main_quit, NULL);
-
-    view = create_view_and_model();
     
-    hbox = gtk_hbox_new(TRUE, 0);
-    button = gtk_button_new_with_label("arst!!");
+    matt_list = g_list_prepend(matt_list, "Matt's Girls");
     
-    gtk_box_pack_start(GTK_BOX(hbox), view, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(hbox), button, TRUE, TRUE, 0);
-
-    gtk_container_add(GTK_CONTAINER(window), hbox);
+    contacts_list = g_list_prepend(contacts_list, "Family");
+    contacts_list = g_list_prepend(contacts_list, "RCOS");
+    contacts_list = g_list_prepend(contacts_list, "RPI Friends");
+    
+    sources = g_hash_table_new(&g_str_hash, &g_str_equal);
+    g_hash_table_insert(sources, "Contacts", contacts_list);
+    g_hash_table_insert(sources, "Matt's Friends", matt_list);
+    
+    gtk_container_add(GTK_CONTAINER(window), create_source_view_test(sources));
 
     gtk_widget_show_all(window);
-
-    sel = gtk_tree_view_get_selection(GTK_TREE_VIEW(view));
-    gtk_tree_selection_unselect_all(sel);
-    
-    gtk_widget_grab_focus(button);
 
     gtk_main();
 
     return 0;
 }
+
