45f3373b751e3d60b94b85faaff9bd8281f6fbfb
Tim Horton <hortont424@gmail.com>
2009-11-05 02:09:05 -0500

    Readd SheepleEDSBackend

--- src/libsheeple/SheepleEDSBackend.c
+++ src/libsheeple/SheepleEDSBackend.c
@@ -1,35 +1,35 @@
 #include <glib.h>
 
-#include "SheepleEDSBackendGroup.h"
+#include "SheepleEDSBackend.h"
 #include "SheepleEDSContact.h"
 #include "sheeple.h"
 
-static void sheeple_eds_backend_group_interface_init (SheepleBackendGroupIface *iface);
+static void sheeple_eds_backend_interface_init (SheepleBackendIface *iface);
 
-G_DEFINE_TYPE_WITH_CODE (SheepleEDSBackendGroup, sheeple_eds_backend_group, G_TYPE_OBJECT,
+G_DEFINE_TYPE_WITH_CODE (SheepleEDSBackend, sheeple_eds_backend, G_TYPE_OBJECT,
                          G_IMPLEMENT_INTERFACE (TYPE_SHEEPLE_BACKEND,
-                                                sheeple_eds_backend_group_interface_init));
+                                                sheeple_eds_backend_interface_init));
 
 static char *
-sheeple_eds_backend_group_get_group_id (SheepleBackendGroup *self)
+sheeple_eds_backend_get_db_id (SheepleBackend *self)
 {
-    return "some-group"; // TODO: implement!
+    return "evolution-data-server";
 }
 
-static SheepleContact *
-sheeple_eds_backend_group_get_contact (SheepleBackendGroup *self, const char *id)
+static SheepleBackendGroup *
+sheeple_eds_backend_get_group (SheepleBackend *self, const char *id)
 {
-    SheepleEDSBackendGroup * backend = SHEEPLE_EDS_BACKEND_GROUP(self);
+    SheepleEDSBackend * backend = SHEEPLE_EDS_BACKEND(self);
     EContact * econtact;
     e_book_get_contact(backend->ebook, id, &econtact, NULL);
     return SHEEPLE_CONTACT(sheeple_eds_contact_new(econtact));
 }
 
 static void
-sheeple_eds_backend_group_interface_init (SheepleBackendGroupIface *iface)
+sheeple_eds_backend_interface_init (SheepleBackendIface *iface)
 {
-    iface->get_group_id = sheeple_eds_backend_group_get_group_id;
-    iface->get_contact = sheeple_eds_backend_group_get_contact;
+    iface->get_db_id = sheeple_eds_backend_get_db_id;
+    iface->get_group = sheeple_eds_backend_get_contact;
 }
 
 void contacts_added_handler (EBookView *ebookview, gpointer added, gpointer self)
@@ -47,7 +47,7 @@ void contacts_added_handler (EBookView *ebookview, gpointer added, gpointer self
 
 void contacts_changed_handler (EBookView *ebookview, gpointer added, gpointer self)
 {
-    GList *list = (GList*)added, *elem;
+    /*GList *list = (GList*)added, *elem;
     EContact * contact;
 
     for(elem = list; elem; elem = elem->next)
@@ -55,12 +55,12 @@ void contacts_changed_handler (EBookView *ebookview, gpointer added, gpointer se
         contact = elem->data;
         g_signal_emit_by_name(self, "contact-changed",
                               e_contact_get(contact, E_CONTACT_UID), NULL);
-    }
+    }*/
 }
 
 void contacts_removed_handler (EBookView *ebookview, gpointer added, gpointer self)
 {
-    GList *list = (GList*)added, *elem;
+    /*GList *list = (GList*)added, *elem;
     EContact * contact;
 
     for(elem = list; elem; elem = elem->next)
@@ -68,7 +68,7 @@ void contacts_removed_handler (EBookView *ebookview, gpointer added, gpointer se
         contact = elem->data;
         g_signal_emit_by_name(self, "contact-removed",
                               e_contact_get(contact, E_CONTACT_UID), NULL);
-    }
+    }*/
 }
 
 void sequence_complete_handler (EBookView *ebookview, gpointer added, gpointer self)
@@ -77,7 +77,7 @@ void sequence_complete_handler (EBookView *ebookview, gpointer added, gpointer s
 }
 
 static void
-sheeple_eds_backend_group_init (SheepleEDSBackendGroup *self)
+sheeple_eds_backend_init (SheepleEDSBackend *self)
 {
     self->ebook = e_book_new_default_addressbook(NULL);
     e_book_open(self->ebook, FALSE, NULL);
@@ -86,19 +86,19 @@ sheeple_eds_backend_group_init (SheepleEDSBackendGroup *self)
     
     e_book_get_book_view(self->ebook, q, NULL, 0, &(self->ebookview), NULL);
     g_signal_connect(self->ebookview, "contacts-added", G_CALLBACK(contacts_added_handler), self);
-    g_signal_connect(self->ebookview, "contacts-changed", G_CALLBACK(contacts_changed_handler), self);
-    g_signal_connect(self->ebookview, "contacts-removed", G_CALLBACK(contacts_removed_handler), self);
+    //g_signal_connect(self->ebookview, "contacts-changed", G_CALLBACK(contacts_changed_handler), self);
+    //g_signal_connect(self->ebookview, "contacts-removed", G_CALLBACK(contacts_removed_handler), self);
     g_signal_connect(self->ebookview, "sequence-complete", G_CALLBACK(sequence_complete_handler), self);
     e_book_view_start(self->ebookview);
 }
 
 static void
-sheeple_eds_backend_group_class_init (SheepleEDSBackendGroupClass *self)
+sheeple_eds_backend_class_init (SheepleEDSBackendClass *self)
 {
 }
 
-SheepleEDSBackendGroup *
-sheeple_eds_backend_group_new ()
+SheepleEDSBackend *
+sheeple_eds_backend_new ()
 {
-    return g_object_new(TYPE_SHEEPLE_EDS_BACKEND_GROUP, NULL);
+    return g_object_new(TYPE_SHEEPLE_EDS_BACKEND, NULL);
 }
--- src/libsheeple/SheepleEDSBackend.h
+++ src/libsheeple/SheepleEDSBackend.h
@@ -1,21 +1,21 @@
-#ifndef __SHEEPLE_EDS_BACKEND_GROUP_H__
-#define __SHEEPLE_EDS_BACKEND_GROUP_H__
+#ifndef __SHEEPLE_EDS_BACKEND_H__
+#define __SHEEPLE_EDS_BACKEND_H__
 
 #include <glib-object.h>
 #include <libebook/e-book.h>
 
-#define TYPE_SHEEPLE_EDS_BACKEND_GROUP             (sheeple_eds_backend_group_get_type ())
-#define SHEEPLE_EDS_BACKEND_GROUP(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), TYPE_SHEEPLE_EDS_BACKEND_GROUP, SheepleEDSBackendGroup))
-#define IS_SHEEPLE_EDS_BACKEND_GROUP(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), TYPE_SHEEPLE_EDS_BACKEND_GROUP))
-#define SHEEPLE_EDS_BACKEND_GROUP_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), TYPE_SHEEPLE_EDS_BACKEND_GROUP, SheepleEDSBackendGroupClass))
-#define IS_SHEEPLE_EDS_BACKEND_GROUP_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), TYPE_SHEEPLE_EDS_BACKEND_GROUP))
-#define SHEEPLE_EDS_BACKEND_GROUP_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), TYPE_SHEEPLE_EDS_BACKEND_GROUP, SheepleEDSBackendGroupClass))
+#define TYPE_SHEEPLE_EDS_BACKEND             (sheeple_eds_backend_get_type ())
+#define SHEEPLE_EDS_BACKEND(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), TYPE_SHEEPLE_EDS_BACKEND, SheepleEDSBackend))
+#define IS_SHEEPLE_EDS_BACKEND(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), TYPE_SHEEPLE_EDS_BACKEND))
+#define SHEEPLE_EDS_BACKEND_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), TYPE_SHEEPLE_EDS_BACKEND, SheepleEDSBackendClass))
+#define IS_SHEEPLE_EDS_BACKEND_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), TYPE_SHEEPLE_EDS_BACKEND))
+#define SHEEPLE_EDS_BACKEND_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), TYPE_SHEEPLE_EDS_BACKEND, SheepleEDSBackendClass))
 
 
-typedef struct _SheepleEDSBackendGroup        SheepleEDSBackendGroup;
-typedef struct _SheepleEDSBackendGroupClass   SheepleEDSBackendGroupClass;
+typedef struct _SheepleEDSBackend        SheepleEDSBackend;
+typedef struct _SheepleEDSBackendClass   SheepleEDSBackendClass;
 
-struct _SheepleEDSBackendGroup
+struct _SheepleEDSBackend
 {
     GObject parent_instance;
     
@@ -23,12 +23,12 @@ struct _SheepleEDSBackendGroup
     EBookView * ebookview;
 };
 
-struct _SheepleEDSBackendGroupClass
+struct _SheepleEDSBackendClass
 {
     GObjectClass parent_class;
 };
 
-SheepleEDSBackendGroup * sheeple_eds_backend_group_new ();
-GType sheeple_eds_backend_group_get_type (void);
+SheepleEDSBackend * sheeple_eds_backend_new ();
+GType sheeple_eds_backend_get_type (void);
 
 #endif
